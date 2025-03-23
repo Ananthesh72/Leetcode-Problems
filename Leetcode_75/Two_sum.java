fbc494debe9b0f0873d8efe9daf4ec8bd375ddae
Ananthesh72 <anantheshshenoy72@gmail.com>
2025-03-23 17:31:07 +0530

    Modify the file two sum

--- Leetcode_75/Two_sum.java
+++ Leetcode_75/Two_sum.java
@@ -1,7 +1,18 @@
-package Leetcode_75;
-
-public record Two_sum() {
+public class Two_sum {
     public static void main(String[] args) {
-        System.out.println("THis is Two sum problem");
+      int[] nums = { 2, 7, 11, 15 };
+      int target = 9;
+      twosum(nums, target);
     }
-}
+  
+    public static int[] twosum(int[] num, int target) {
+      for (int i = 0; i < num.length; i++) {
+        for (int j = i + 1; j < num.length; j++) {
+          if (target == num[i] + num[j]) {
+            return new int[] { i, j };
+          }
+        }
+      }
+      return null;
+    }
+  }
