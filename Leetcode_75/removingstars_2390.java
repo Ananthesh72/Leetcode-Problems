e4a661cd8476e15821fa15b42cf310a6b4b96200
Ananthesh72 <https://github.com/Ananthesh72/Leetcode-Problems.git>
2025-04-14 19:55:34 +0530

    Added Leetcode problem Removing Stars 2390

---
+++ Leetcode_75/removingstars_2390.java
@@ -0,0 +1,23 @@
+package Leetcode;
+public class removingstars_2390{
+
+    public static String removeStars(String str){
+        StringBuilder result = new StringBuilder();
+
+        for(char ch : str.toCharArray()){
+            if(ch =='*'){
+                if(result.length() >0){
+                    result.deleteCharAt(result.length() -1);
+                }
+            }else{
+                result.append(ch);
+                System.out.println(result);
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String str="leet**cod*e";
+        System.out.println(removeStars(str));
+    }
+}
