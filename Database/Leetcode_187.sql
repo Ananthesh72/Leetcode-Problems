8612581ef75b77569da104e93fbb02047b7d7635
Ananthesh72 <anantheshshenoy72@gmail.com>
2025-06-29 22:21:28 +0530

    Database Leetcode 187

---
+++ Database/Leetcode_187.sql
@@ -0,0 +1,7 @@
+#Leetcode 187. Rank Scores
+SELECT s.score,
+       (SELECT COUNT(DISTINCT score)
+        FROM Scores
+        WHERE score >= s.score) AS rank
+FROM Scores s
+ORDER BY s.score DESC;
