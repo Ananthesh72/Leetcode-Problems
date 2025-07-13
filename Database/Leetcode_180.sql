81c8102f6c970e09e7f30715667d6599e5f7f840
Ananthesh72 <anantheshshenoy72@gmail.com>
2025-07-13 22:19:57 +0530

    Database Leetcode 180

--- Database/Leetcode_180.sql
+++ Database/Leetcode_180.sql
@@ -4,7 +4,7 @@ FROM (
   SELECT
     num,
     LAG(num, 1) OVER (ORDER BY id) AS prev_num,
-    LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    LEAD(num, 1) OVER (ORDER BY id) AS next_n
   FROM Logs
 ) t
 WHERE num = prev_num AND num = next_num;
