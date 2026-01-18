3bc0c0d25e265ee8cbab3e6a1e241f55ea4f4758
Ananthesh72 <anantheshshenoy72@gmail.com>
2026-01-18 21:16:56 +0530

    Added CodingBatt Array problems

---
+++ Codingbatt/Array1/firstLast6.java
@@ -0,0 +1,21 @@
+package Codingbatt.Array1;
+
+public class firstLast6 {
+    public static void main(String[] args) {
+
+        firstLast6 obj = new firstLast6();
+        int nums[] = { 13, 6, 1, 2, 6 };
+        System.out.println(obj.firstLast6(nums));
+    }
+
+    public boolean firstLast6(int[] nums) {
+        if (nums.length == 0) {
+            return false;
+        }
+        if (nums[0] == 6 || nums[nums.length - 1] == 6) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Codingbatt/Array1/sameFirstLast.java
@@ -0,0 +1,16 @@
+package Codingbatt.Array1;
+
+public class sameFirstLast {
+    public static void main(String[] args) {
+        sameFirstLast obj = new sameFirstLast();
+        int nums[] = { 1, 2, 3 ,1};
+        System.out.println(obj.sameFirstLast1(nums));
+    }
+
+    public boolean sameFirstLast1(int[] nums) {
+        if (nums.length >= 1 && nums[0] == nums[nums.length - 1]) {
+            return true;
+        }
+        return false;
+    }
+}
